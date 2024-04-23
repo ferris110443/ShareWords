910c5a22900267e6393b8289d15ce3e9a6dc3fbd
ferris110443 <ferris110443@hotmail.com.tw>
2024-04-23 18:21:49 +0800

    <fix> : modify btn css

--- quill/quill.js
+++ quill/quill.js
@@ -164,3 +164,49 @@ function updatePreview(markdownText) {
     document.getElementById("preview-text").innerHTML = htmlContent; // Display the HTML in the preview div
 }
 
+
+//================upload Image ============================
+// document.getElementById('editor').addEventListener('paste', function (event) {
+//     let clipboardData = event.clipboardData || window.clipboardData;
+//     if (clipboardData && clipboardData.items) { //clipboardData.items is a list of all the items that were copied (text,image, etc.
+//         for (let i = 0; i < clipboardData.items.length; i++) {
+//             let item = clipboardData.items[i];
+//             console.log('Item type: ', item.type);
+//             console.log('Item type indexOf ', item.type.indexOf('image'));
+//             if (item.type.indexOf('image') === 0) {  // Check if the item is an image
+//                 let blob = item.getAsFile();
+//                 let reader = new FileReader();
+//                 reader.onload = function (e) {
+//                     // console.log('File content: ', e.target.result);
+//                     uploadImage(e.target.result).then(url => {
+//                         insertImageMarkdown(url);
+//                     });
+//                 };
+//                 reader.readAsDataURL(blob);
+//                 event.preventDefault(); // Prevent the default behavior of pasting
+//             }
+//         }
+//     }
+// });
+//
+//
+// function insertImageMarkdown(url) {
+//     const markdownText = `![Image](${url})`;
+//     editor.insert(markdownText);
+// }
+//
+// async function uploadImage(data) {
+//     try {
+//         const response = await fetch('api/1.0/upload/Image', {
+//             method: 'POST',
+//             body: JSON.stringify({image: data}),
+//             headers: {'Content-Type': 'application/json'}
+//         });
+//         const responseData = await response.json();
+//         // console.log("responseData" + JSON.stringify(responseData));
+//         return responseData["imageURL"];
+//     } catch (error) {
+//         console.error("Error uploading image:", error);
+//         return null;
+//     }
+// }
