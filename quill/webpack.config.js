1e52ae2dc6b6ee22d16c53130a47b74683124bda
ferris110443 <ferris110443@hotmail.com.tw>
2024-05-08 18:47:13 +0800

    <fix> : nohup bug

--- quill/webpack.config.js
+++ quill/webpack.config.js
@@ -1,20 +1,21 @@
 const path = require('path')
 
 module.exports = {
-  mode: 'development',
-  devtool: 'source-map',
-  entry: {
-    quill: './quill.js'
-  },
-  output: {
-    globalObject: 'self',
-    path: path.resolve(__dirname, './dist/'),
-    filename: '[name].bundle.js',
-    publicPath: '/dist/'
-  },
-  devServer: {
-    port:3000,
-    compress: true,
-    static: '.'
-  }
+    mode: 'development',
+    devtool: 'source-map',
+    entry: {
+        quill: './quill.js'
+    },
+    output: {
+        globalObject: 'self',
+        path: path.resolve(__dirname, './dist/'),
+        filename: '[name].bundle.js',
+        publicPath: '/dist/'
+    },
+    devServer: {
+        allowedHosts: 'all',
+        port: 3000,
+        compress: true,
+        static: '.'
+    }
 }
