569ff3484de7ce63a416c6c3c27e0b58d788b7a0
ferris110443 <ferris110443@hotmail.com.tw>
2024-05-13 14:14:08 +0800

    <fix> : fix authentication

--- src/main/resources/static/src/pages/workspace.js
+++ src/main/resources/static/src/pages/workspace.js
@@ -6,13 +6,11 @@ const roomId = decodeURIComponent(queryParams.get('roomId'));
 const roomNumber = params.get('roomNumber');
 const coeditorURL = 'https://sharewords.org/coeditor';
 // const coeditorURL = 'http://localhost:8888/coeditor';
-
-if (await checkAuthentication()) {
-    renderWorkspaceFileList()
-    renderWorkspaceInformation()
-    getWorkspaceMember()
-    getUserFriendsForAddingMembers()
-}
+checkAuthentication
+renderWorkspaceFileList()
+renderWorkspaceInformation()
+getWorkspaceMember()
+getUserFriendsForAddingMembers()
 
 
 document.addEventListener('DOMContentLoaded', function () {
